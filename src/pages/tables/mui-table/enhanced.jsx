f82448416820b585e2452c34ff4d5d1a2e8128fe
codemaster1022 <codemaster.han1022127@gmail.com>
2024-10-06 16:14:58 -0700

    url fix

--- src/pages/tables/mui-table/enhanced.jsx
+++ src/pages/tables/mui-table/enhanced.jsx
@@ -171,7 +171,7 @@ export default function EnhancedTable() {
   useEffect(() => {
     const fetchDexItem = async () => {
       try {
-        const response = await axios.get('http://dex-admin-view.vercel.app');  // Fetch the data
+        const response = await axios.get('https://dex-backend.vercel.app/');  // Fetch the data
         console.log(response.data);
         setDexItem(response.data);  // Update state with the fetched data
       } catch (err) {
