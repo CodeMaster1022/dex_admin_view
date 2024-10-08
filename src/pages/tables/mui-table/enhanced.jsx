a8dca7f2fe04409f59d0cbebe5299d7d7755c8a2
codemaster1022 <codemaster.han1022127@gmail.com>
2024-10-08 01:03:32 -0700

    information added

--- src/pages/tables/mui-table/enhanced.jsx
+++ src/pages/tables/mui-table/enhanced.jsx
@@ -20,7 +20,7 @@ import { toast } from 'react-toastify';
 import MainCard from 'components/MainCard';
 import Typography from '@mui/material/Typography';
 // import { CSVExport, RowSelection } from 'components/third-party/react-table';
-
+import "../../../CSS/loading.css"
 // table data
 function createData(number,address, total, profit, rate, buy,token) {
   return {
@@ -33,7 +33,17 @@ function createData(number,address, total, profit, rate, buy,token) {
     token
   };
 }
+const formatDate = (dateString) => {
+  const date = new Date(dateString);
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-indexed
+  const day = String(date.getDate()).padStart(2, '0');
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const seconds = String(date.getSeconds()).padStart(2, '0');
 
+  return `${year}.${month}.${day} ${hours}:${minutes}:${seconds}`;
+};
 // table data
 const rows = [
   createData(1,'Cupcake', 305, 3.7, 67, 4.3,2),
@@ -119,19 +129,9 @@ function EnhancedTableHead({ onSelectAllClick, order, orderBy, numSelected, rowC
   };
 
   return (
+    <>
     <TableHead>
       <TableRow>
-        {/* <TableCell padding="checkbox" sx={{ pl: 3 }}>
-          <Checkbox
-            color="primary"
-            indeterminate={numSelected > 0 && numSelected < rowCount}
-            checked={rowCount > 0 && numSelected === rowCount}
-            onChange={onSelectAllClick}
-            inputProps={{
-              'aria-label': 'select all desserts'
-            }}
-          />
-        </TableCell> */}
         {headCells.map((headCell) => (
           <TableCell
             key={headCell.id}
@@ -153,6 +153,7 @@ function EnhancedTableHead({ onSelectAllClick, order, orderBy, numSelected, rowC
         ))}
       </TableRow>
     </TableHead>
+    </>
   );
 }
 
@@ -168,13 +169,26 @@ export default function EnhancedTable() {
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
   const [selectedValue, setSelectedValue] = React.useState([]);
   const [dexItem, setDexItem] = React.useState([]);
+  const [dateItem, setDateItem] = React.useState('');
+  const [loading, setLoading] = React.useState(true);
+  const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
   useEffect(() => {
     const fetchDexItem = async () => {
       try {
+        setLoading(true);
+        // const response = await axios.get('https://dex-backend.vercel.app/');  // Fetch the data
         const response = await axios.get('https://dex-backend.vercel.app/');  // Fetch the data
-        setDexItem(response.data);  // Update state with the fetched data
+        const responseDate = await axios.get('https://dex-backend.vercel.app/date');  // Fetch the data
+        console.log(responseDate)
+        if(response.data&&responseDate.data){
+          setDexItem(response.data);
+          setDateItem(responseDate.data);
+          await delay(300);
+          setLoading(false);
+        }  // Update state with the fetched data
       } catch (err) {
-        setError(err);  // Set error state if the request fails
+        setLoading(false)
+        alert("There is no any data");  // Set error state if the request fails
       } 
     };
 
@@ -204,25 +218,6 @@ export default function EnhancedTable() {
     }
     setSelected([]);
   };
-
-  // const handleClick = (event, name) => {
-  //   const selectedIndex = selected.indexOf(name);
-  //   let newSelected = [];
-
-  //   if (selectedIndex === -1) {
-  //     newSelected = newSelected.concat(selected, name);
-  //   } else if (selectedIndex === 0) {
-  //     newSelected = newSelected.concat(selected.slice(1));
-  //   } else if (selectedIndex === selected.length - 1) {
-  //     newSelected = newSelected.concat(selected.slice(0, -1));
-  //   } else if (selectedIndex > 0) {
-  //     newSelected = newSelected.concat(selected.slice(0, selectedIndex), selected.slice(selectedIndex + 1));
-  //   }
-  //   const selectedRowData = dexItem.filter((row) => newSelected.includes(row.name.toString()));
-  //   setSelectedValue(selectedRowData);
-  //   setSelected(newSelected);
-  // };
-
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
   };
@@ -237,104 +232,104 @@ export default function EnhancedTable() {
   // Avoid a layout jump when reaching the last page with empty rows.
   const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - dexItem.length) : 0;
 
-  return (
+  return ( 
+  <>
+    {loading?
+      <div class="loader">Loading
+        <span></span>
+      </div>:    
     <MainCard
       content={false}
-      title="Top profit 200"
-      // secondary={<CSVExport data={selectedValue.length > 0 ? selectedValue : rows} filename={'enhanced-table-data.csv'} />}
     >
-      {/* <RowSelection selected={selected.length} /> */}
+      <Typography sx={{ml:3, py:'10px',fontSize:'20px',color:'gray'}}>Profit Top 200</Typography>
+      <Typography sx={{ml:3, display:'flex'}}>
+        <Typography color={"#6D9EEB"}>Token Traded in</Typography>&nbsp;:&nbsp;
+        {/* <Typography sx={{color:'#ABE796'}}>MC</Typography>&nbsp;&nbsp; */}
+        <Typography sx={{color:"#ABE796"}}>5M</Typography>&nbsp;&nbsp;
+        <Typography sx={{color:'#E7B114'}}>7D</Typography>&nbsp;
+        UTC&nbsp;[&nbsp;{formatDate(dateItem[0].startTime)}&nbsp;~&nbsp;{formatDate(dateItem[0].endTime)}&nbsp;]
+      </Typography>
+    <TableContainer>
+      <Table sx={{ minWidth: 750 }} aria-labelledby="tableTitle" size={dense ? 'small' : 'medium'}>
+        <EnhancedTableHead
+          numSelected={selected.length}
+          order={order}
+          orderBy={orderBy}
+          onSelectAllClick={handleSelectAllClick}
+          onRequestSort={handleRequestSort}
+          rowCount={dexItem.length}
+        />
+        <TableBody>
+          {stableSort(dexItem, getComparator(order, orderBy))
+            .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+            .map((row, index) => {
+              if (typeof row === 'number') return null;
+              const isItemSelected = isSelected(row.name);
+              const labelId = `enhanced-table-checkbox-${index}`;
 
-      {/* table */}
-      <TableContainer>
-        <Table sx={{ minWidth: 750 }} aria-labelledby="tableTitle" size={dense ? 'small' : 'medium'}>
-          <EnhancedTableHead
-            numSelected={selected.length}
-            order={order}
-            orderBy={orderBy}
-            onSelectAllClick={handleSelectAllClick}
-            onRequestSort={handleRequestSort}
-            rowCount={dexItem.length}
-          />
-          <TableBody>
-            {stableSort(dexItem, getComparator(order, orderBy))
-              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              .map((row, index) => {
-                if (typeof row === 'number') return null;
-                const isItemSelected = isSelected(row.name);
-                const labelId = `enhanced-table-checkbox-${index}`;
-
-                return (
-                  <TableRow
-                    hover
-                    // onClick={(event) => handleClick(event, row.name)}
-                    role="checkbox"
-                    aria-checked={isItemSelected}
-                    tabIndex={-1}
-                    key={row.id}
-                    selected={isItemSelected}
-                  >
-                    {/* <TableCell sx={{ pl: 3 }} padding="checkbox">
-                      <Checkbox
-                        color="primary"
-                        checked={isItemSelected}
-                        inputProps={{
-                          'aria-labelledby': labelId
-                        }}
-                      />
-                    </TableCell> */}
-                    <TableCell align="left">{index+1}</TableCell>
-                    <TableCell component="th" id={labelId} scope="row" padding="none" style={{ width: '40%' }}>
-                        <Typography fontSize={12}>Profit Top 200 Dynamic:&nbsp;{row.Address}</Typography>
-                    </TableCell>
-                    <TableCell align="left">
-                        <Typography sx={{backgroundColor:'#094B0C', px:'8px', fontSize:'12px', color:'#5F9A64', display:'inline-block'}}>
-                        {row.Total_PNL ? row.Total_PNL.toFixed(2) : '0.00'}x
-                        </Typography>
-                    </TableCell>
-                    <TableCell align="left" ><Typography color='#D9A23B' fontSize={12}>{row.Total_Profit ? row.Total_Profit.toFixed(2) : '0.00'}</Typography></TableCell>
-                    <TableCell align="center">
-                      <Typography fontSize={12}>
-                      {row.Win_Rate ? row.Win_Rate.toFixed(2) : '0.00'}%
+              return (
+                <TableRow
+                  hover
+                  role="checkbox"
+                  aria-checked={isItemSelected}
+                  tabIndex={-1}
+                  key={row.id}
+                  selected={isItemSelected}
+                >
+                  <TableCell align="left">{index+1}</TableCell>
+                  <TableCell component="th" id={labelId} scope="row" padding="none" style={{ width: '40%' }}>
+                      <Typography fontSize={12}>Profit Top 200 Dynamic:&nbsp;{row.Address}</Typography>
+                  </TableCell>
+                  <TableCell align="left">
+                      <Typography sx={{backgroundColor:'#094B0C', px:'8px', fontSize:'12px', color:'#5F9A64', display:'inline-block'}}>
+                      {row.Total_PNL ? row.Total_PNL.toFixed(2) : '0.00'}x
                       </Typography>
-                      <Box sx={{display:'flex',justifyContent:'center',gap:'5px'}}>
-                          <Typography fontSize={9}>Win&nbsp;:&nbsp;{row.Token_win_traded}</Typography>
-                          <Typography fontSize={9}>Lose&nbsp;:&nbsp;{row.Token_Traded-row.Token_win_traded}</Typography>
-                      </Box>
-                    </TableCell>
-                    <TableCell sx={{ pr: 3 }} align="center">
-                      {row.Avg_Buy_Price ? row.Avg_Buy_Price.toFixed(2) : '0.00'}
-                    </TableCell>
-                    <TableCell sx={{ pr: 3}} align="right">
-                      <Box sx={{minWidth:'150px'}}> 
-                        {row.Token_Traded}&nbsp;&nbsp;<Button onClick={() => copyToClipboard(row)}><Typography sx={{backgroundColor:'yellow',borderRadius:'10px',px:'2px',fontSize:'12px'}}>&nbsp;Copy&nbsp;</Typography></Button>
-                      </Box>
-                    </TableCell>
-                  </TableRow>
-                );
-              })}
-            {emptyRows > 0 && (
-              <TableRow sx={{ height: (dense ? 33 : 53) * emptyRows }}>
-                <TableCell colSpan={6} />
-              </TableRow>
-            )}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <Divider />
+                  </TableCell>
+                  <TableCell align="left" ><Typography color='#D9A23B' fontSize={12}>{row.Total_Profit ? row.Total_Profit.toFixed(2) : '0.00'}</Typography></TableCell>
+                  <TableCell align="center">
+                    <Typography fontSize={12}>
+                    {row.Win_Rate ? row.Win_Rate.toFixed(2) : '0.00'}%
+                    </Typography>
+                    <Box sx={{display:'flex',justifyContent:'center',gap:'5px'}}>
+                        <Typography fontSize={9}>Win&nbsp;:&nbsp;{row.Token_win_traded}</Typography>
+                        <Typography fontSize={9}>Lose&nbsp;:&nbsp;{row.Token_Traded-row.Token_win_traded}</Typography>
+                    </Box>
+                  </TableCell>
+                  <TableCell sx={{ pr: 3 }} align="center">
+                    {row.Avg_Buy_Price ? row.Avg_Buy_Price.toFixed(2) : '0.00'}
+                  </TableCell>
+                  <TableCell sx={{ pr: 3}} align="right">
+                    <Box sx={{minWidth:'150px'}}> 
+                      {row.Token_Traded}&nbsp;&nbsp;<Button onClick={() => copyToClipboard(row)}><Typography sx={{backgroundColor:'#141414',borderRadius:'10px',px:'2px',fontSize:'12px'}}>&nbsp;Copy&nbsp;</Typography></Button>
+                    </Box>
+                  </TableCell>
+                </TableRow>
+              );
+            })}
+          {emptyRows > 0 && (
+            <TableRow sx={{ height: (dense ? 33 : 53) * emptyRows }}>
+              <TableCell colSpan={6} />
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </TableContainer>
+    <Divider />
 
-      {/* table pagination */}
-      <TablePagination
-        rowsPerPageOptions={[5, 10, 25]}
-        component="div"
-        count={dexItem.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
-      />
-    </MainCard>
+    {/* table pagination */}
+    <TablePagination
+      rowsPerPageOptions={[5, 10, 25]}
+      component="div"
+      count={dexItem.length}
+      rowsPerPage={rowsPerPage}
+      page={page}
+      onPageChange={handleChangePage}
+      onRowsPerPageChange={handleChangeRowsPerPage}
+    />
+    </MainCard>}   
+</>
   );
+
 }
 
 EnhancedTableHead.propTypes = {
